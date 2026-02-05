cdf38d2e65d467a026630fdec02c891db24f0c58
kharysawulandari <539251141@student.smktelkom-jkt.sch.id>
2026-02-05 12:06:59 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import random
+
+welcome_message = "WELCOME TO CUYPY GAMES!"
+cuypy_position = random.randint(1, 4)
+
+print("*****************************")
+print(f"** {welcome_message} **")
+print("*****************************")
+
+nama_user = input("masukan nama anda: ")
+
+print(f'''
+Halo {nama_user}! Coba perhatikan goa dibawah ini
+|_| |_| |_| |_|
+''')
+
+pilihan_user = int(input("Menurut kamu di goa nomor berapa CUYPY berada? [1 / 2 / 3 / 4]: "))
+
+print(f"pilihan kamu adalah {pilihan_user}")
+
+if pilihan_user == cuypy_position:
+    print(f"Selamat {nama_user} kamu menang! posisi CUYPY ada di goa nomor {cuypy_position} dan pilihanmu adalah goa nomor {pilihan_user}.")
+else:
+    print(f"KAMU KALAH! cuypy bukan berada disitu, tapi ada di goa nomor {cuypy_position}. Sedangkan kamu memilih goa nomor {pilihan_user}")
